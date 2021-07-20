bb081e730aaecdfd6002a23b260d523166ad2020
陈阳苹 <7722368+chen_yangping@user.noreply.gitee.com>
2021-07-20 11:39:30 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,8 @@ class SessionsController extends Controller
         ]);
         if (Auth::attempt($credentials)) {
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
@@ -30,5 +31,12 @@ class SessionsController extends Controller
         Auth::logout();
         session()->flash('success', '您已成功退出！');
         return redirect('login');
+    }
+
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
     }
 }
