2cec8e104d140a390101622ff44dcaedaa6a3644
陈阳苹 <7722368+chen_yangping@user.noreply.gitee.com>
2021-09-07 20:44:15 +0800

    修改密码123456

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,7 +10,8 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        //'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$LoAFQtUTj88i1j2tjy5okuet5oATP03xLK9siBUNtJ2osVGztpwgm',
         'remember_token' => Str::random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
