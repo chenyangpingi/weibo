82b4c9a37beaca62a3707a13295e73b8a5dc4fdd
陈阳苹 <7722368+chen_yangping@user.noreply.gitee.com>
2021-09-07 20:26:22 +0800

    修改1密码++

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,16 +15,11 @@ class SessionsController extends Controller
     {
         $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
-//            'password' => 'required'
+            'password' => 'required'
         ]);
-        if (Auth::attempt($credentials)) {
-            session()->flash('success', '欢迎回来！');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
-        } else {
-            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back()->withInput();
-        }
+        session()->flash('success', '欢迎回来！');
+        $fallback = route('users.show', Auth::user());
+        return redirect()->intended($fallback);
     }
     public function destroy()
     {
